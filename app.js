0d0b91676e43698bb1f9ef98c0cd57d40aa15926
Raphael Chookagian <raphaelchookagian@gmail.com>
2022-09-21 22:35:19 -0700

    adding more styling fixes

--- app.js
+++ app.js
@@ -1,46 +1,49 @@
 "use strict";
 
+let days = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
 
-let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-
-let open = '6:00 am to 8:00 PM'; 
-let closed = 'Sorry we\'re closed';
+let open = "6:00 am to 8:00 PM";
+let closed = "Sorry we're closed";
 
 //Welcome
-let welcome = document.getElementById('Welcome');
-let welcomeHeading = document.createElement('h2');
-welcomeHeading.textContent = 'Welcome to Pat\'s Salmon Cookies!';
+let welcome = document.getElementById("Welcome");
+let welcomeHeading = document.createElement("h2");
+welcomeHeading.textContent = "Welcome to Pat's Salmon Cookies!";
 welcome.appendChild(welcomeHeading);
 
-let intro = document.getElementById('Welcome');
-let intropara = document.createElement('p');
-intropara.textContent = 'Pat\'s Salmon Cookies started as a small corner stand by Pat himself, way back in the day. His family has kept thier family recipe for generations, and now they want to share their delicious cookies with the world! Come on in and check out our variety of gluten free, animal and kid friendly cookies! ';
+let intro = document.getElementById("Welcome");
+let intropara = document.createElement("p");
+intropara.textContent =
+  "Pat's Salmon Cookies started as a small corner stand by Pat himself, way back in the day. His family has kept their family recipe for generations, and now they want to share their delicious cookies with the world! Come on in and check out our variety of gluten free, animal and kid friendly cookies! ";
 intro.appendChild(intropara);
 
-
-
-
-
 // Hours
-let hoursOpen = document.getElementById('Hours');
-let hoursHeading = document.createElement('h2');
-hoursHeading.textContent = 'Hours of Operation';
+let hoursOpen = document.getElementById("Hours");
+let hoursHeading = document.createElement("h2");
+hoursHeading.textContent = "Hours of Operation";
 hoursOpen.appendChild(hoursHeading);
 
-let dailyHours = document.getElementById('Hours');
-let dailyHoursOpen = document.createElement('ul');
+let dailyHours = document.getElementById("Hours");
+let dailyHoursOpen = document.createElement("ul");
 dailyHours.appendChild(dailyHoursOpen);
 
-for (let i = 0; i < days.length-1; i++) {
-  let listItem = document.createElement('li');
+for (let i = 0; i < days.length - 1; i++) {
+  let listItem = document.createElement("li");
   listItem.textContent = `${days[i]}: ${open}`;
   dailyHoursOpen.appendChild(listItem);
 }
 
-let closedSun = document.createElement('li');
+let closedSun = document.createElement("li");
 closedSun.textContent = `${days.pop()}: ${closed}`;
 dailyHoursOpen.appendChild(closedSun);
-
 
 // 6:00 AM to 8:00 PM for all locations
 let hours = [
@@ -103,7 +106,9 @@ ShopLocations.prototype.custPerHour = function () {
 
 ShopLocations.prototype.cookiesPerHr = function () {
   for (let i = 0; i < this.customer.length; i++) {
-    this.hrlyTotal[i] = Math.round(this.customer[i] * this.averageCookiesPerCust);
+    this.hrlyTotal[i] = Math.round(
+      this.customer[i] * this.averageCookiesPerCust
+    );
     this.dailyTotal += this.hrlyTotal[i];
     console.log(this.hrlyTotal[i]);
   }
@@ -158,16 +163,16 @@ function createFooter() {
 
   //------
   let grandTotal = 0;
-  
+
   for (let i = 0; i < hours.length; i++) {
     let tdElem = document.createElement("td");
     let hrlyTotal = 0;
-    
+
     for (let j = 0; j < allCookieCafes.length; j++) {
       hrlyTotal += allCookieCafes[j].hrlyTotal[i];
-      grandTotal+= allCookieCafes[j].hrlyTotal[i];
+      grandTotal += allCookieCafes[j].hrlyTotal[i];
     }
-    
+
     tdElem.textContent = hrlyTotal;
     trElem.appendChild(tdElem);
   }
@@ -175,7 +180,6 @@ function createFooter() {
   trElem.appendChild(td1Elem);
   td1Elem.textContent = grandTotal;
 }
-
 
 //----------------------------
 
@@ -199,6 +203,3 @@ allCookieCafes[4].render();
 
 createFooter();
 //createFooter();
-
-
-
